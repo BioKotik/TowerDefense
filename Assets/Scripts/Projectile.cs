c930770ea33bc7d09ada40e94ace08d5ebad06c1
ilyakachan <i.kachan.work@gmail.com>
2021-06-19 13:41:07 +0300

    Create Tower and Projectile. Make upgrade script for tower and projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float damage;
+
+    public void Attack(Transform target)
+    {
+        StartCoroutine(MoveToTarget(target));
+    }
+
+    private IEnumerator MoveToTarget(Transform target)
+    {
+        float t = 0;
+        Vector3 start = transform.position;
+        while (t < 1)
+        {
+            t += Time.deltaTime * moveSpeed;
+
+            transform.position = Vector3.Lerp(start, target.position, t);
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Tower/Tower.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private float attackSpeed;
+    [SerializeField] private float attackRange;
+    [SerializeField] private Projectile projectilePrefab;
+    [SerializeField] private GameObject target;
+    [SerializeField] private TowerUpgrades upgrade;
+
+    private void Init()
+    {
+        attackSpeed += upgrade.attackSpeedBonus;
+        attackRange += upgrade.attackRangeBonus;
+        projectilePrefab = upgrade.projectileBonus ? upgrade.projectileBonus : projectilePrefab;
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    public void Attack(Transform target)
+    {
+        Instantiate(projectilePrefab, transform.position, Quaternion.identity).Attack(target);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && CheckRange(target.transform))
+        {
+            Attack(target.transform);
+        }
+    }
+
+    private bool CheckRange(Transform target)
+    {
+        if (Vector3.Distance(transform.position, target.position) <= attackRange)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
